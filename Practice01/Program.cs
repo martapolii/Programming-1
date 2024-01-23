70f8d21b6574354d333499106998d806f2581054
Marta Polishchuk <mpolish3@my.centennialcollege.ca>
2024-01-22 22:12:07 -0500

    Created console application that asks for your name.

---
+++ Practice01/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Greeting
+{
+    static void Main(string[] args)
+    {
+        Console.Write("What is your name? ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Hello, " + name + "! Nice to meet you.");
+    }
+}
